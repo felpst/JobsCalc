9ee68197ebf53db2daebf0cfb5313c4499d0ebb0
Felipe Veloso <felipe.dearaujo@snhu.edu>
2021-06-01 13:08:30 -0300

    Second day conclusion

--- src/routes.js
+++ src/routes.js
@@ -5,16 +5,31 @@ const views = __dirname + '/views/' //dirname da o endereço absoluto do server.
 
 const profile = {
     name: "Felps",
-    avatar: "https://avatars.githubusercontent.com/u/61332644?v=4",
+    avatar: "https://www.github.com/felpst.png",
     "monthly-budget": 3000,
     "days-per-week": 5,
     "hours-per-day": 5,
     "vacation-per-year": 4
 }
 
+const jobs = []
+
 // req, res. Toda vez que o server pegar (get) uma / ele vai executar a função que tem os parâmetros req (qual é o pedido do usuário) e reponse (qual a resposta dada pelo servidor).
 routes.get('/', (req, res) => res.render(views + "index")) 
 routes.get('/job', (req, res) => res.render(views + "job"))
+routes.get('/job', (req, res) => {
+    // req.body = {name: 'something', 'daily-hours': '3.1', 'total-hours': '3'}
+
+    const job = req.body
+    job.createdAt = Date.now(); // Atribuindo uma nova data
+
+    jobs.push({
+        name: req.body.name,
+        "daily-hours": req.body["daily-hours"],
+        "total-hours": req.body["total-hours"],
+    })
+    return res.redirect('/') // Estou redirecionando para o / que é o index
+})
 routes.get('/job/edit', (req, res) => res.render(views + "job-edit"))
 routes.get('/profile', (req, res) => res.render(views + "profile", { profile}))
 
--- src/server.js
+++ src/server.js
@@ -2,10 +2,13 @@ const express = require("express")
 const server = express()
 const routes = require("./routes") // Isto está pegando as rotas que estão no arquvio routes.js
 
-// Usando template envie (ou view engine)
+// Usando template engine (ou view engine)
 server.set('view engine', 'ejs') // Isto daqui é o motor que fará a leitura do código html. Estou usando o padrão de interpretação ejs, que me possibilita colocar código JS dentro do HTML
 
-server.use(express.static("public")) // O use serve para você criar configurações ao seu servidor. Neste caso ele está sendo usado para ditar como o servidor deve fazer as rotas (usando os arquivos do diretório público). Este use crie um Midware (O que é isso?)
+server.use(express.static("public")) // O use serve para você criar configurações no seu servidor. Neste caso ele está sendo usado para ditar como o servidor deve fazer as rotas (usando os arquivos do diretório público). Este use crie um Midware (O que é isso?)
+
+// Usar o req.body
+server.use(express.urlencoded({ extended: true }))
 
 // Routes
 server.use(routes) // Aqui o servidor está usando as rodas pegas acima do arquivo routes.js
